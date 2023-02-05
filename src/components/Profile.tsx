b877bbaf38034417ec77f13ed07de276efac9a17
KaftyrevaPolina <pkaft.13.8@gmail.com>
2023-02-05 23:09:17 +0300

    1-13 add grid

---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img
+                src='https://media.istockphoto.com/id/647197172/photo/vintage-photo-of-lavender-field.jpg?s=612x612&w=0&k=20&c=lMRdg0O_UzJTIv7yC6OdHb1cbNMg0xpj1FtVjBmlNj8='/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+
+}
+
