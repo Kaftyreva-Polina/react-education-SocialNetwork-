0c60cf65f4aaabcc4234e34c0bd77a7d0c04d489
KaftyrevaPolina <pkaft.13.8@gmail.com>
2023-04-01 23:52:49 +0300

    23 add props

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import s from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
-export const Dialogs = (props: any) => {
+type MessageType = {
+    message: string
+}
+
+type DialogsItemType = {
+    id: number
+    name: string
+}
+const DialogsItem = (props: DialogsItemType) => {
+    let path = "/dialogs/" + props.id
+    return <div className={s.dialog}>
+        <NavLink to={path}>{props.name}</NavLink>
+    </div>
+}
+
+const Message = (props: MessageType) => {
+    return <div className={s.message}>{props.message}</div>
+}
+
+export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <DialogsItem id={1} name={"Dmitriy"}/>
+                <DialogsItem id={2} name={"Andrey"}/>
+                <DialogsItem id={3} name={"Svetlana"}/>
+                <DialogsItem id={4} name={"Sasha"}/>
+                <DialogsItem id={5} name={"Viktor"}/>
+                <DialogsItem id={6} name={"Valera"}/>
+            </div>
+            <div className={s.messages}>
+                <Message message={"Hello"}/>
+                <Message message={"Hi"}/>
+                <Message message={"How are you?"}/>
+            </div>
+
+
         </div>
     )
 }
